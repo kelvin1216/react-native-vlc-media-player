beb6f4bdb5c11a2b200aa008d4027cc91ad0ebdc
Kelvin Lan <kelvin6129@gmail.com>
2020-06-01 15:31:53 +0800

    Fixed mute issue

--- ios/RCTVLCPlayer/RCTVLCPlayer.m
+++ ios/RCTVLCPlayer/RCTVLCPlayer.m
@@ -261,6 +261,48 @@ static NSString *const playbackRate = @"rate";
         [_player jumpForward:interval];
 }
 
+/**
+ * audio  -----> start
+ */
+- (void)setMuted:(BOOL)muted
+{
+    if(_player){
+        VLCAudio *audio = _player.audio;
+        [audio setMuted: muted];
+    }
+}
+
+-(void)setVolume:(int)interval
+{
+    if(_player){
+        VLCAudio *audio = _player.audio;
+        if(interval >= 0){
+            audio.volume = interval;
+        }
+    }
+}
+
+-(void)setVolumeDown:(int)volume
+{
+    if(_player){
+
+        VLCAudio *audio = _player.audio;
+        [audio volumeDown];
+    }
+}
+
+
+
+-(void)setVolumeUp:(int)volume
+{
+    if(_player){
+        VLCAudio *audio = _player.audio;
+        [audio volumeUp];
+    }
+}
+
+//audio  -----> end
+
 -(void)setSeek:(float)pos
 {
     if([_player isSeekable]){
